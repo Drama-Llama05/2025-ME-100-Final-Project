1d561c802498b831728212bcab4dfc5fd14ef006
bienerp <bienerp@berkeley.edu>
2025-05-06 21:01:02 -0700

    Update buzzer.py
    alarm() function

--- IR_Buzzer_Host/buzzer.py
+++ IR_Buzzer_Host/buzzer.py
@@ -8,7 +8,7 @@ class Buzzer:
         self.pin = pin
         print(f"Buzzer initialized on pin {self.pin}")
 
-    def alert(self, freq=300, duty=700, duration=1):
+    def alert(self, freq=300, duty=700, duration=0.25):
         """
         - freq: PWM frequency in Hz
         - duty: duty cycle (0–1023 for 10‑bit PWM)
@@ -19,5 +19,16 @@ class Buzzer:
         buzzer_pwm.duty(duty)
         time.sleep(duration)
         buzzer_pwm.deinit()
-
-
+        
+    def alarm(self):
+        buzzer_pwm = PWM(Pin(self.pin))
+        for freq in range(300, 1201, 4):
+            buzzer_pwm.freq(freq)
+            time.sleep(0.01)
+                
+            # Gradually decrease the frequency from 1500Hz back down to 300Hz.
+        for freq in range(1200, 299, -4):
+           buzzer_pwm.freq(freq)
+           time.sleep(0.01)
+                
+        buzzer_pwm.deinit()
